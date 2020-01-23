7594671ec3ea314acc99c61f231e8065b1b7a386
Jeff Horner <jhorner3208@gmail.com>
2020-01-23 08:10:06 -0600

    add masthead background image to div container

---
+++ src/components/LandingPageMasthead.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="containerMasthead">
+        Test
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+.containerMasthead {
+    background-image: url("https://live.staticflickr.com/65535/49429246967_37a8b3ce00_o.jpg");
+    background-position: center -16rem;
+    background-repeat:  no-repeat;
+    background-attachment: fixed;
+    background-size:  cover;
+    height: 90vh;
+    widht: 100%;
+}
+
+</style>
